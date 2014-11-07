c56980148400623e292e16939daf4ef9f1be1ff5
foo <foo@email.com>
2014-11-07 17:05:29 -0300

    province app

---
+++ practice/province-app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Province App - Informatorio 2014</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <!-- <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script> -->
+    </head>
+    <body>
+        <div class="container">
+            <div class="divsearch">
+                <input type="text" name="search" placeholder="Search..." size="40">
+                <input type="button" value="Search">
+            </div>
+            <hr>
+        </div>  
+    </body>
+</html>
