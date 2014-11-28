0fcc026a048aebe6a2e27314bfc7a110b0816d38
Float <Float@192.168.1.84>
2014-11-28 17:34:55 -0300

    28/11

--- practice/province-app/index.php
+++ practice/province-app/index.php
@@ -3,27 +3,32 @@
     <head>
         <title>Province App - Informatorio 2014</title>
         <link rel="stylesheet" type="text/css" href="css/main.css">
-        
     </head>
     <body>
         <div class="container">
             <div class="searchbox">
-                <input type="text" name="inputsearch" placeholder="Search..." id="inputsearch">
-                <input type="button" id="searchbuton" value="Search">
-            </div>
+                <form class="form-inline" role="form">
+                    <div class="form-group">
+                      <div class="input-group">
+                        <input class="form-control" type="text" name="inputsearch" placeholder="Search..." id="inputsearch">
+                      </div>
+                    </div>
+                    <input type="button" class="btn btn-default" id="searchbuton" value="Search">
+                  </form>
+            </div>  
             <div class="content">
                 <div class="provinces">
-                    <h2>PROVINCES</h2>
-                    <ul></ul>
+                    <h3>PROVINCES</h3>
+                    <ul class="list-group"></ul>
                 </div>
                 <div class="cities">
-                    <h2>CITIES</h2>
-                    <ul></ul>
+                    <h3>CITIES</h3>
+                    <ul class="list-group"></ul>
                 </div>
             </div>
         </div>
         <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/main.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
     </body>
 </html>
--- practice/province-app/js/main.js
+++ practice/province-app/js/main.js
@@ -1,53 +1,54 @@
-$(document).ready(function(){
-    $(function(){
-    $("#inputsearch").keyup(function(){
-        var value = $(this).val();
-        if (value.length >= 2){
-            $.ajax({
-                url: 'filter_provinces.php',
-                data: {"data": value},
-                success: function(response){
-                    console.log(response);
-                    $('.provinces ul').html('');
-                    $('.cities ul').html('');
-                    $.each(response, function(key, province){
-                        updateProvinces(province)
-                    });
-                    if (response.length != 0){
-                        $('.provinces ul').append('<li data-count= ' + response.length + '>' + response.length + '</li>');
-                    } else {
+$(document).ready(function () {
+    $(function () {
+        $("#inputsearch").keyup(function () {
+            var value = $(this).val();
+            if (value.length >= 2) {
+                $.ajax({
+                    url: 'filter_provinces.php',
+                    data: {"data": value},
+                    success: function (response) {
+                        console.log(response);
                         $('.provinces ul').html('');
-                        $('.provinces ul').append('<p>No se encontraron resultados</p>')
+                        $('.cities ul').html('');
+                        $.each(response, function (key, province) {
+                            updateProvinces(province, value)
+                        });
+                        if (response.length != 0) {
+                            $('.provinces ul').append('<button type="button" class="btn btn-primary">' + response.length + '</button>');
+                        } else {
+                            $('.provinces ul').html('');
+                            $('.provinces ul').append('<p>No se encontraron resultados</p>')
+                        }
+                    }
+                })
+            }
+            ;
+        })
+        function updateProvinces(province, value) {
+            $('.provinces ul').append('<li data-id= "' + province.id + '" class="list-group-item">' + '<b>' + (province.name).substr(0, value.length) + '</b>' + (province.name).substr(value.length) + '</li>');
+        }
+        $('.provinces ul').on('click', 'li', function () {
+            var id = $(this).data('id');
+            $.ajax({
+                url: 'filter_cities.php',
+                data: {"id": id},
+                success: function (response) {
+                    console.log(response);
+                    $('.cities ul').html('')
+                    $.each(response, function (key, city) {
+                        updateCities(city)
+                    });
+                    if (response.length != 0) {
+                        $('.cities ul').append('<button type="button" class="btn btn-primary">' + response.length + '</button>');
+                    } else {
+                        $('.cities ul').html('');
+                        $('.cities ul').append('<p>No se encontraron ciudades para esta provincia</p>')
                     }
                 }
             })
-        };
-    })
-    function updateProvinces(province){
-        $('.provinces ul').append('<li data-id= "'+province.id+'">' + province.name + '</li>');
-    }
-    $('.provinces ul').on('click', 'li', function(){
-        var id = $(this).data('id');
-        $.ajax({
-            url: 'filter_cities.php',
-            data: {"id": id},
-            success: function(response){
-                console.log(response);
-                $('.cities ul').html('')
-                $.each(response, function(key, city){
-                    updateCities(city)
-                });
-                if (response.length != 0){
-                        $('.cities ul').append('<li data-count= ' + response.length + '>' + response.length + '</li>');
-                } else {
-                    $('.cities ul').html('');
-                    $('.cities ul').append('<p>No se encontraron ciudades para esta provincia</p>')
-                }
+            function updateCities(city) {
+                $('.cities ul').append('<li data-id= "' + city.id + '" class="list-group-item">' + city.name + '</li>');
             }
         })
-    function updateCities(city){
-        $('.cities ul').append('<li data-id= "'+city.id+'">' + city.name + '</li>');
-    }    
     })
-})                                               
 })
